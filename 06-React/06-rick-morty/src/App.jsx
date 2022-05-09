4eb3754fae245887f4ef09ab9a48efcc2b8f5c02
Ivan Ramalho Tonial <81388283+ivantonial@users.noreply.github.com>
2022-05-09 16:43:38 -0300

    att
    att

--- 06-React/06-rick-morty/src/App.jsx
+++ 06-React/06-rick-morty/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
     (async () => {
       setLoading(true);
       let allChars = [];
-      for (let i = 1; i < 8; i++) {
+      for (let i = 1; i < 42; i++) {
         const reponse = await fetch(
           'https://rickandmortyapi.com/api/character/?page=' + i,
         );
