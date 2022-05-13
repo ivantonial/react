31953524f3973818651b02bcd61c5e44e39a3c7f
Ivan Tonial <ivantonial@gmail.com>
2022-05-12 22:29:39 -0300

    att

--- 07-rick-morty/src/main.jsx
+++ 07-rick-morty/src/main.jsx
@@ -3,7 +3,7 @@
 // import App from './App'
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
